52df42f15c4e87c5a5b7034d2778bc55620141a1
rowjay007 <rowlives@gmail.com>
2021-03-10 10:05:53 +0100

    create Booklog, Home, SearchBar and BookShelf components

---
+++ src/components/BookLog.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BookLog = () => {
+  return <div />;
+};
+
+export default BookLog;
---
+++ src/components/BookShelf.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BookShelf() {
+  return <div />;
+}
+
+export default BookShelf;
